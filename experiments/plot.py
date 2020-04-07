a7a62d3e0071bc526973673adcd86941a1228fd0
Lars Larsson <larsson@cs.umu.se>
2020-04-07 10:57:08 +0200

    Load sausages in separate subfigs

--- experiments/plot.py
+++ experiments/plot.py
@@ -28,27 +28,25 @@ def parse_directory(results_directory):
 
 
 def plot_network_traffic(received_bytes, transmitted_bytes):
-    fig, ax = plt.subplots(3, 1, sharex=True)
+    fig, ax = plt.subplots(3, 1, sharex=True, sharey=True)
 
-    recv_ax = ax[0]
-    recv_ax.plot(received_bytes.index, received_bytes['static-0_received_bytes'], color='black', label='Static TTL (0)')
-    recv_ax.plot(received_bytes.index, received_bytes['dynamic-adaptive-0.1_received_bytes'], color='blue', label='Adaptive TTL (0.1)')
-    recv_ax.plot(received_bytes.index, received_bytes['dynamic-updaterisk-0.1_received_bytes'], color='orange', label='Update-risk based (0.1)')
+    fig.suptitle('Network traffic (bytes/sec)')
 
-    recv_ax.plot(transmitted_bytes.index, -transmitted_bytes['static-0_transmitted_bytes'], color='black', label='Static TTL (0)')
-    recv_ax.plot(transmitted_bytes.index, -transmitted_bytes['dynamic-adaptive-0.1_transmitted_bytes'], color='blue', label='Adaptive TTL (0.1)')
-    recv_ax.plot(transmitted_bytes.index, -transmitted_bytes['dynamic-updaterisk-0.1_transmitted_bytes'], color='orange', label='Update-risk based (0.1)')
+    static_ax = ax[0]
+    static_ax.plot(transmitted_bytes.index, transmitted_bytes['static-0_transmitted_bytes'], color='black', label='Static TTL (0)')
+
+    adaptive_ax = ax[1]
+    adaptive_ax.plot(transmitted_bytes.index, transmitted_bytes['dynamic-adaptive-0.1_transmitted_bytes'], color='blue', label='Adaptive TTL (0.1)')
+
+    updaterisk_ax = ax[2]
+    updaterisk_ax.plot(transmitted_bytes.index, transmitted_bytes['dynamic-updaterisk-0.1_transmitted_bytes'], color='orange', label='Update-risk based (0.1)')
 
     fig.legend()
 
-    xmit_ax = ax[1]
-    xmit_ax.plot(transmitted_bytes.index, -transmitted_bytes['static-0_transmitted_bytes'], color='black', label='Static TTL (0)')
-    xmit_ax.plot(transmitted_bytes.index, -transmitted_bytes['dynamic-adaptive-0.1_transmitted_bytes'], color='blue', label='Adaptive TTL (0.1)')
-    xmit_ax.plot(transmitted_bytes.index, -transmitted_bytes['dynamic-updaterisk-0.1_transmitted_bytes'], color='orange', label='Update-risk based (0.1)')
-
+    static_ax.plot(received_bytes.index, -received_bytes['static-0_received_bytes'], color='black', label='Static TTL (0)')
+    adaptive_ax.plot(received_bytes.index, -received_bytes['dynamic-adaptive-0.1_received_bytes'], color='blue', label='Adaptive TTL (0.1)')
+    updaterisk_ax.plot(received_bytes.index, -received_bytes['dynamic-updaterisk-0.1_received_bytes'], color='orange', label='Update-risk based (0.1)')
     plt.show()
-
-
 
 if __name__=="__main__":
     results_directory = sys.argv[1]
