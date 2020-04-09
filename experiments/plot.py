fa99df19ff5ca10dda3cbdf29c990b96609e5c06
Lars Larsson <larsson@cs.umu.se>
2020-04-09 09:02:39 +0200

    Legend and title placement

--- experiments/plot.py
+++ experiments/plot.py
@@ -5,13 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
+from matplotlib.backends.backend_pdf import PdfPages
+
 from parse import parse_directory
 
 
 def plot_network_traffic(received_bytes, transmitted_bytes):
-    fig, ax = plt.subplots(3, 1, sharex=True, sharey=True)
+    fig, ax = plt.subplots(3, 1, sharex=True, sharey=True, constrained_layout=True)
 
-    fig.suptitle('Network traffic (bytes/sec)')
+    fig.suptitle('Network traffic (bytes/sec)\n\n')
 
     static_ax = ax[0]
     static_ax.plot(transmitted_bytes['relative_timestamp'], transmitted_bytes['static-0_transmitted_bytes'], color='black')
@@ -26,13 +28,16 @@ def plot_network_traffic(received_bytes, transmitted_bytes):
     adaptive_ax.plot(received_bytes['relative_timestamp'], -received_bytes['dynamic-adaptive-0.1_received_bytes'], color='black')
     updaterisk_ax.plot(received_bytes['relative_timestamp'], -received_bytes['dynamic-updaterisk-0.1_received_bytes'], color='black')
 
-    static_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['static-0_received_bytes'], transmitted_bytes['static-0_transmitted_bytes'], facecolor='white', hatch='||', label='Static TTL (0)')
-    adaptive_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['dynamic-adaptive-0.1_received_bytes'], transmitted_bytes['dynamic-adaptive-0.1_transmitted_bytes'], facecolor='white', hatch='//', label='Adaptive TTL (0.1)')
-    updaterisk_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['dynamic-updaterisk-0.1_received_bytes'], transmitted_bytes['dynamic-updaterisk-0.1_transmitted_bytes'], facecolor='white', hatch='\\\\', label='Update-risk based (0.1)')
+    static_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['static-0_received_bytes'], transmitted_bytes['static-0_transmitted_bytes'], facecolor='white', edgecolor='black', hatch='||', label='Static TTL (0)')
+    adaptive_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['dynamic-adaptive-0.1_received_bytes'], transmitted_bytes['dynamic-adaptive-0.1_transmitted_bytes'], facecolor='white', hatch='//', edgecolor='black', label='Adaptive TTL (0.1)')
+    updaterisk_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['dynamic-updaterisk-0.1_received_bytes'], transmitted_bytes['dynamic-updaterisk-0.1_transmitted_bytes'], facecolor='white', edgecolor='black', hatch='\\\\', label='Update-risk based (0.1)')
 
-    fig.legend()
+    fig.legend(ncol=3, bbox_to_anchor=(0.98, 0.95), frameon=False)
 
     plt.show()
+
+    fig.savefig('hipsterShopNetworkTraffic.pdf', bbox_inches='tight')
+
 
 
 if __name__=="__main__":
