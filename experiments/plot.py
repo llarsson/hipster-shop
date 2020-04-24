a9a4077e1113ed692ae237239bdd265a5636311f
Lars Larsson <larsson@cs.umu.se>
2020-04-24 11:13:50 +0200

    Legends fixed for sequential sausages

--- experiments/plot.py
+++ experiments/plot.py
@@ -38,9 +38,9 @@ def plot_network_traffic_sausages(received_bytes, transmitted_bytes):
 
 
 def plot_network_traffic_sequential_sausages(received_bytes, transmitted_bytes):
-    fig, ax = plt.subplots(1, 3, figsize=(6.4, 2), sharex=True, sharey=True, constrained_layout=True)
+    fig, ax = plt.subplots(1, 3, figsize=(6.5, 2), sharex=True, sharey=True, constrained_layout=True)
 
-    fig.suptitle('Network traffic (bytes/sec)\n')
+    fig.suptitle('Network traffic (bytes/sec)')
 
     static_ax = ax[0]
     static_ax.plot(transmitted_bytes['relative_timestamp'], transmitted_bytes['static-0_transmitted_bytes'], color='black')
@@ -59,7 +59,12 @@ def plot_network_traffic_sequential_sausages(received_bytes, transmitted_bytes):
     adaptive_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['dynamic-adaptive-0.1_received_bytes'], transmitted_bytes['dynamic-adaptive-0.1_transmitted_bytes'], facecolor='white', hatch='//', edgecolor='black', label='Adaptive TTL (0.1)')
     updaterisk_ax.fill_between(received_bytes['relative_timestamp'], -received_bytes['dynamic-updaterisk-0.1_received_bytes'], transmitted_bytes['dynamic-updaterisk-0.1_transmitted_bytes'], facecolor='white', edgecolor='black', hatch='\\\\', label='Update-risk based (0.1)')
 
-    fig.legend(ncol=3, bbox_to_anchor=(0.98, 0.91), frameon=False)
+    #fig.legend(ncol=3, bbox_to_anchor=(0.98, 0.91), frameon=False)
+    #fig.legend(ncol=3, mode='expand', bbox_to_anchor=(0.1, 0.91, 0.9, 0), frameon=False)
+
+    static_ax.legend(loc='upper center', bbox_to_anchor=(0.31, 1.25), frameon=False)
+    adaptive_ax.legend(loc='upper center', bbox_to_anchor=(0.4, 1.25), frameon=False)
+    updaterisk_ax.legend(loc='upper center', bbox_to_anchor=(0.48, 1.25), frameon=False)
 
     fig.savefig('hipsterShopNetworkTraffic-sequential.pdf', bbox_inches='tight')
 
@@ -101,10 +106,12 @@ if __name__=="__main__":
 
     print(received_bytes)
 
-    plot_network_traffic_sausages(received_bytes, transmitted_bytes)
+    plt.rc('font', size=8)
+
+    #plot_network_traffic_sausages(received_bytes, transmitted_bytes)
 
     plot_network_traffic_sequential_sausages(received_bytes, transmitted_bytes)
 
-    plot_network_traffic_overlapping(received_bytes, transmitted_bytes)
+    #plot_network_traffic_overlapping(received_bytes, transmitted_bytes)
 
     plt.show()
